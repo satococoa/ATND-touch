2cbfed50d3b6dba38935721e75bc6134a244a02b
Satoshi Ebisawa <e.satoshi@gmail.com>
2010-10-09 21:40:56 +0900

    initial commit

---
+++ atnd-touch.js
@@ -0,0 +1,14 @@
+var jQT = new $.jQTouch({
+  icon: 'atnd-touch-icon.png',
+  startupScreen: 'atnd-touch-startup.png',
+  statusBar: 'black-translucent',
+  preloadImages: [
+    'themes/jqt/img/chevron_white.png',
+    'themes/jqt/img/bg_row_select.gif',
+    'themes/jqt/img/back_button_clicked.png',
+    'themes/jqt/img/button_clicked.png'
+  ]
+});
+$(function(){
+  window.scrollTo(0, 0);
+});
