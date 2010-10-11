22ff572fb3b1ea954b2fea865f10672f47d62ed5
Satoshi Ebisawa <e.satoshi@gmail.com>
2010-10-11 09:55:40 +0900

    twitterログイン機能を実装

--- atnd-touch.js
+++ atnd-touch.js
@@ -9,6 +9,49 @@ var jQT = new $.jQTouch({
     'themes/jqt/img/button_clicked.png'
   ]
 });
+
 $(function(){
   window.scrollTo(0, 0);
+
+  // ログインしていなければ、ログインを促す
+  twttr.anywhere(function(T) {
+    var currentUser, screenName, profileImage, profileImage, profileImageTag;
+
+    var authorized = function(user) {
+      currentUser = user;
+      screenName = currentUser.data('screen_name');
+      profileImage = currentUser.data('profile_image_url');
+      profileImageTag = '<img width="16" height="16" src="' + profileImage + '"/>';
+      $('#settings').removeClass('current');
+      $('#home').addClass('current');
+      $('#logout').show();
+    };
+
+    var unauthorized = function() {
+      $('#settings').addClass('current');
+      $('#logout').hide();
+    };
+
+    if (T.isConnected()) {
+      authorized(T.currentUser);
+    } else {
+      unauthorized();
+    }
+
+    T.bind('authComplete', function(e, user) {
+      authorized(user);
+    });
+
+    T.bind('signOut', function(e) {
+      unauthorized();
+    });
+
+    T('#loginButton').connectButton();
+    $('#logout').tap(function(e) {
+      twttr.anywhere.signOut();
+    });
+    $('#logout').click(function(e) {
+      twttr.anywhere.signOut();
+    });
+  });
 });
